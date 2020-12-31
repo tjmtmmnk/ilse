883476eb6ff67cdade3c6599b75899cebcbe272b
tjmtmmnk <tsujimoto027@gmail.com>
2020-12-31 22:25:50 +0900

    change preview range

--- preview.go
+++ preview.go
@@ -5,7 +5,6 @@ import (
 	"os/exec"
 	"strconv"
 
-	"github.com/gdamore/tcell/v2"
 	"github.com/rivo/tview"
 	"github.com/tjmtmmnk/ilse/filter"
 )
@@ -18,19 +17,16 @@ func initPreview() {
 	preview = tview.NewTextView().
 		SetDynamicColors(true).
 		SetScrollable(true)
-
-	preview.SetBackgroundColor(tcell.ColorDefault)
 }
 
 func getPreviewContent(item filter.SearchResult) (string, error) {
 	_, h := app.screen.Size()
-	from := item.Highlight - (h/2 - 1)
+	from := item.LineNum - (h/2 - 1)
 	if from < 0 {
 		from = 0
 	}
-	to := item.Highlight + (h/2 + 1)
-	lineRange := fmt.Sprintf("%d:%d", from, to)
-	cmd := []string{"bat", "--line-range", lineRange, "--highlight-line", strconv.Itoa(item.Highlight), "--color=always", "--theme", app.config.theme, "--style=numbers", item.FileName}
+	lineRange := fmt.Sprintf("%d:", from)
+	cmd := []string{"bat", "--line-range", lineRange, "--highlight-line", strconv.Itoa(item.LineNum), "--color=always", "--theme", app.config.theme, "--style=numbers,changes", item.FileName}
 
 	out, err := exec.Command(cmd[0], cmd[1:]...).Output()
 	return string(out), err
