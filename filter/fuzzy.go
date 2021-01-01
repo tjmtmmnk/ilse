e87222bfbf68371f17f8b01a994b3588ea42200a
tjmtmmnk <tsujimoto027@gmail.com>
2021-01-02 02:36:06 +0900

    ignore binary file in fuzzy search

--- filter/fuzzy.go
+++ filter/fuzzy.go
@@ -2,15 +2,16 @@ package filter
 
 import (
 	"io/ioutil"
+	"net/http"
 	"os"
 	"path/filepath"
-	"sort"
+	"strings"
 	"sync"
 
 	"github.com/sahilm/fuzzy"
 )
 
-type Fuzzy struct{}
+type fuzzySearch struct{}
 
 type searchResultWithScore struct {
 	*SearchResult
@@ -22,14 +23,19 @@ type file struct {
 	text string
 }
 
-func (f *Fuzzy) Search(q string, option *SearchOption) ([]SearchResult, error) {
+func (f *fuzzySearch) Search(q string, option *SearchOption) ([]SearchResult, error) {
 	var (
-		results          []SearchResult
-		resultsWithScore []searchResultWithScore
-		texts            []string
+		results []SearchResult
+		texts   []string
 	)
 
+	length := len(q)
+
 	files := f.getFiles()
+
+	if len(files) == 0 {
+		return []SearchResult{}, nil
+	}
 
 	for _, file := range files {
 		texts = append(texts, file.text)
@@ -37,34 +43,27 @@ func (f *Fuzzy) Search(q string, option *SearchOption) ([]SearchResult, error) {
 
 	matches := fuzzy.Find(q, texts)
 	for _, m := range matches {
-		for _, idx := range f.reIndex(m.MatchedIndexes, len(q)) {
+		for _, idx := range f.reIndex(m.MatchedIndexes, length) {
 			fileName := files[m.Index].name
-			lineNum, text := f.getLine(files[m.Index].name, idx+len(q))
+			lineNum, text := f.getLine(files[m.Index].name, idx+length)
 			if text == "" {
 				continue
 			}
 			result := SearchResult{fileName, lineNum, text}
-			resultsWithScore = append(resultsWithScore, searchResultWithScore{&result, m.Score})
+			results = append(results, result)
 		}
-	}
-	sort.Slice(resultsWithScore, func(i, j int) bool {
-		return resultsWithScore[i].score > resultsWithScore[j].score
-	})
-
-	for _, res := range resultsWithScore {
-		results = append(results, *res.SearchResult)
 	}
 
 	return results, nil
 }
 
-func (f *Fuzzy) getLine(fileName string, pos int) (int, string) {
+func (f *fuzzySearch) getLine(fileName string, pos int) (int, string) {
 	text, _ := ioutil.ReadFile(fileName)
 	lineNum := 1
 	lineText := ""
 	to := pos
-	if pos > len(text) {
-		pos = len(text) - 1
+	if to > len(text) {
+		to = len(text) - 1
 	}
 	for _, c := range text[:to] {
 		lineText += string(c)
@@ -77,12 +76,13 @@ func (f *Fuzzy) getLine(fileName string, pos int) (int, string) {
 	return lineNum, lineText
 }
 
-func (f *Fuzzy) getFiles() []file {
+func (f *fuzzySearch) getFiles() []file {
 	var wg sync.WaitGroup
-	var mu sync.RWMutex
+	var mu sync.Mutex
 	var files []file
 	fileNames, _ := f.getFileNames()
 	for _, name := range fileNames {
+
 		wg.Add(1)
 		go func(name string) {
 			defer wg.Done()
@@ -90,6 +90,12 @@ func (f *Fuzzy) getFiles() []file {
 			if err != nil {
 				return
 			}
+			mimeType := http.DetectContentType(text)
+			isBinary := !strings.HasPrefix(mimeType, "text/plain")
+			if isBinary {
+				return
+			}
+
 			mu.Lock()
 			files = append(files, file{name, string(text)})
 			mu.Unlock()
@@ -98,16 +104,15 @@ func (f *Fuzzy) getFiles() []file {
 	wg.Wait()
 	return files
 }
-
-func (f *Fuzzy) reIndex(indexes []int, queryLength int) []int {
+func (f *fuzzySearch) reIndex(indexes []int, queryLength int) []int {
 	if len(indexes) == 0 {
 		return []int{}
 	}
 	baseIdx := indexes[0]
 	reIndexes := []int{baseIdx}
 	for _, idx := range indexes {
-		isSameIndex := idx <= baseIdx+queryLength
-		if isSameIndex {
+		isSameWord := idx <= baseIdx+queryLength
+		if isSameWord {
 			continue
 		}
 		reIndexes = append(reIndexes, idx)
@@ -116,7 +121,7 @@ func (f *Fuzzy) reIndex(indexes []int, queryLength int) []int {
 	return reIndexes
 }
 
-func (f *Fuzzy) getFileNames() ([]string, error) {
+func (f *fuzzySearch) getFileNames() ([]string, error) {
 	var fileNames []string
 	err := filepath.Walk(".", func(path string, info os.FileInfo, err error) error {
 		if info.IsDir() {
