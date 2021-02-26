83fc35e8a7b3fd43ece683ced6ee0e2031cfabf1
tjmtmmnk <tsujimoto027@gmail.com>
2021-02-27 04:51:28 +0900

    don't need filter/util

--- filter/ripgrep.go
+++ filter/ripgrep.go
@@ -1,7 +1,10 @@
 package filter
 
 import (
+	"errors"
 	"os/exec"
+	"strconv"
+	"strings"
 
 	"github.com/tjmtmmnk/ilse/util"
 )
@@ -10,6 +13,49 @@ type rg struct{}
 
 func newRg() *rg {
 	return &rg{}
+}
+
+func isValidQuery(q string) bool {
+	return q != ""
+}
+
+func isValidRegex(q string) bool {
+	return true
+}
+
+func convert(result string, option *SearchOption) ([]SearchResult, error) {
+	results := make([]SearchResult, 0, option.Limit)
+	for i, s := range strings.Split(result, "\n") {
+		if i > option.Limit {
+			break
+		}
+		res, err := split(s, option)
+		if err != nil {
+			return []SearchResult{}, err
+		}
+		if res != nil {
+			results = append(results, *res)
+		}
+	}
+	return results, nil
+}
+
+func split(str string, option *SearchOption) (*SearchResult, error) {
+	// first remove reset flag included in path, line
+	str = strings.Replace(str, "\x1b[0m", "", 4)
+	splitted := strings.Split(str, ":")
+	if len(splitted) < 3 {
+		return nil, nil
+	}
+
+	fileName := splitted[0]
+	lineNum, err := strconv.Atoi(splitted[1])
+	if err != nil {
+		return nil, errors.New("line number wrong format")
+	}
+	// change reset flag included in text to black foreground
+	text := strings.ReplaceAll(splitted[2], "\x1b[0m", "\x1b[39;40m")
+	return &SearchResult{fileName, lineNum, text}, nil
 }
 
 func (r *rg) Search(q string, option *SearchOption) ([]SearchResult, error) {
